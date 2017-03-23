cc0273aba1e1bf3cda4260337599ae0e6ddd5473
Federico Naranjo <federico.naranjo@gmail.com>
2017-03-23 13:37:21 -0400

    Timer App
    One of my first apps, learning how to make a simple UI and trigger
    actions with buttons

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var interval = 0.01
+    var currentTime = 0.0
+    var go = false, hasStarted = false, running = false
+
+
+
+    @IBOutlet var label: UILabel!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var stopButton: UIButton!
+
+    @IBAction func playPause(sender: UIButton) {
+        
+        if(!running) {
+            timer = NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: #selector(ViewController.increaseTime), userInfo: nil, repeats: true)
+            hasStarted = true
+            running = true
+            sender.setTitle("Pause", forState: .Normal)
+        }
+        else {
+            timer.invalidate()
+            running = false
+            sender.setTitle("Play", forState: .Normal)
+            
+        }
+        
+    }
+
+    @IBAction func stop(sender: UIButton) {
+        timer.invalidate()
+        running = false
+        currentTime = 0.0
+        label.text = "0.0"
+    }
+    
+    
+    func increaseTime() {
+        currentTime += interval
+        label.text = String(format: "%0.2f",currentTime)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +66,3 @@ class ViewController: UIViewController {
 
 
 }
-
